545b60d7059a440383de822c3932aa4228886553
pjj <15705547960@163.com>
2021-04-26 22:26:35 +0800

    fix

--- public/static/js/js.js
+++ public/static/js/js.js
@@ -1,21 +1,21 @@
 $(function () {
   $('#wordnum1').blur(function () {
-    var price1 = $('#wordnum1').val() * 0.35
+    var price1 = $('#wordnum1').val() * 0.06
     $('#result1').val(price1)
   })
 
   $('#wordnum2').blur(function () {
-    var price2 = $('#wordnum2').val() * 0.45
+    var price2 = $('#wordnum2').val() * 0.075
     $('#result2').val(price2)
   })
 
   $('#wordnum3').blur(function () {
-    var price3 = $('#wordnum3').val() * 0.5
+    var price3 = $('#wordnum3').val() * 1
     $('#result3').val(price3)
   })
 
   $('#wordnum4').blur(function () {
-    var price4 = $('#wordnum4').val() * 0.2
+    var price4 = $('#wordnum4').val() * 0.03
     $('#result4').val(price4)
   })
 
