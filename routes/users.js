98449a468c567a87c3da0a39003ac139621d8e8a
pjj <15705547960@163.com>
2021-05-06 09:43:11 +0800

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var utils = require('../utils');
-const sendEmail = require('../utils//sendEmail.js');
+const sendEmail = require('../utils/sendEmail.js');
+
+var fs = require('fs');
+var multer  = require('multer');
+var path = require('path');
+
 
 var db = require('./db') //引入数据库封装模块
 /* GET users listing. */
@@ -97,5 +102,27 @@ router.post('/login', function (req, res, next) {
     }
   })
 })
+var upload = multer({dest: 'upload_tmp/'});
+
+router.post('/upload', upload.any(), function(req, res, next) {
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = "./upload/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+            }else{
+                response = {
+                    message:'File uploaded successfully',
+                    filename:req.files[0].originalname
+                };
+                console.log( response );
+                res.end( JSON.stringify( response ) );
+            }
+        });
+    });
+});
+
 
 module.exports = router;
